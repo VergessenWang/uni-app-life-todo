89b95fa070ed5385b55ff2af6af80961ee3704b3
vergessenwang <469294979@qq.com>
2021-02-28 23:04:01 +0800

    build: 初始化项目及uniCloud

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,49 +1,36 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view class="content">
+    <!-- text==span  view==div -->
+    <view> <text class="title">1111</text><text class="title">222</text> </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+  data() {
+    return {};
+  },
+  onLoad() {},
+  methods: {},
+};
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style lang="scss">
+/* page==body */
+page {
+  /* background: pink; */
+}
+/* 单位 px rox rem vh vw */
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .title {
+    font-size: 36rpx;
+    color: #8f8f94;
+  }
+}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
---
+++ src/pages/my/my.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		mypage
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		},
+		onTabItemTap(e) {
+			console.log(e)
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
